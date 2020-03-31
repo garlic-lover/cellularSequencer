93b94ad77feff77269948e437c5886d49a01aa6b
Matthieu Poupinet <matthieupoupinet@gmail.com>
2020-03-31 15:26:54 +0200

    scales added

--- src/assets/scales.js
+++ src/assets/scales.js
@@ -1,7 +1,23 @@
 const major = ["C", "D", "E", "F", "G", "A", "B"];
 
+const harmonic_major = ["C", "D", "E", "F", "G", "G#", "B"];
+
 const minor = ["C", "D", "D#", "F", "G", "A", "A#"];
+
+const lydian = ["C", "D", "E", "F#", "G", "A", "B"];
+
+const mixolydian = ["C", "D", "E", "F", "G", "A", "A#"];
 
 const arabic = ["C", "C#", "E", "F", "G", "G#", "B"];
 
-export default { major, minor, arabic };
+const hungarian_gypsy = ["C", "D", "D#", "F#", "G", "G#", "B"];
+
+export default {
+  major,
+  harmonic_major,
+  minor,
+  lydian,
+  mixolydian,
+  arabic,
+  hungarian_gypsy
+};
