ca5d8bfe76c584328ced606aa0712bdeeb6109c2
Matthieu Poupinet <matthieupoupinet@gmail.com>
2020-04-03 18:28:41 +0200

    drums added

--- src/Synth/SynthContainer.js
+++ src/Synth/SynthContainer.js
@@ -22,6 +22,46 @@ import Height_hihatO from "../assets/Samples/808/808hihatO.wav";
 import Height_conga from "../assets/Samples/808/808conga.wav";
 import Height_clap from "../assets/Samples/808/808clap.wav";
 
+// import of 909
+
+import Nine_kick from "../assets/Samples/909/bd.wav";
+import Nine_snare from "../assets/Samples/909/snare.wav";
+import Nine_snare2 from "../assets/Samples/909/snare2.wav";
+import Nine_ride from "../assets/Samples/909/ride.wav";
+import Nine_hat from "../assets/Samples/909/hat.wav";
+import Nine_clave from "../assets/Samples/909/clave.wav";
+import Nine_clap from "../assets/Samples/909/clap.wav";
+
+// import of LinnDrum
+
+import Linn_kick from "../assets/Samples/LinnDrum/bd.wav";
+import Linn_snare from "../assets/Samples/LinnDrum/snare.wav";
+import Linn_conga from "../assets/Samples/LinnDrum/conga.wav";
+import Linn_conga2 from "../assets/Samples/LinnDrum/conga2.wav";
+import Linn_hat from "../assets/Samples/LinnDrum/hat.wav";
+import Linn_clave from "../assets/Samples/LinnDrum/clave.wav";
+import Linn_cow from "../assets/Samples/LinnDrum/cow.wav";
+
+// import of Kraftwerk
+
+import Kraft_kick from "../assets/Samples/Kraftwek/bd.wav";
+import Kraft_snare from "../assets/Samples/Kraftwek/snare.wav";
+import Kraft_tom from "../assets/Samples/Kraftwek/tom.wav";
+import Kraft_tom2 from "../assets/Samples/Kraftwek/tom2.wav";
+import Kraft_noise from "../assets/Samples/Kraftwek/noise.wav";
+import Kraft_clave from "../assets/Samples/Kraftwek/clave.wav";
+import Kraft_clap from "../assets/Samples/Kraftwek/clap.wav";
+
+// import of Industrial
+
+import Indus_kick from "../assets/Samples/Industrial/bd.wav";
+import Indus_tom from "../assets/Samples/Industrial/tom.wav";
+import Indus_indus from "../assets/Samples/Industrial/indus.wav";
+import Indus_scratch from "../assets/Samples/Industrial/scratch.wav";
+import Indus_hat from "../assets/Samples/Industrial/hat.wav";
+import Indus_clap2 from "../assets/Samples/Industrial/clap2.wav";
+import Indus_clap from "../assets/Samples/Industrial/clap.wav";
+
 import ParametersList from "./ParametersList";
 
 //Import of scales
@@ -71,7 +111,35 @@ class SynthConainer extends React.Component {
     F3: hit,
     G3: rim,
     A3: hihat,
-    "A#3": hihat
+    "A#3": hihat,
+    C4: Nine_kick,
+    D4: Nine_snare,
+    "D#4": Nine_snare2,
+    F4: Nine_ride,
+    G4: Nine_hat,
+    A4: Nine_clap,
+    "A#4": Nine_clave,
+    C5: Linn_kick,
+    D5: Linn_snare,
+    "D#5": Linn_conga,
+    F5: Linn_conga2,
+    G5: Linn_hat,
+    A5: Linn_cow,
+    "A#5": Linn_clave,
+    C6: Kraft_kick,
+    D6: Kraft_snare,
+    "D#6": Kraft_tom,
+    F6: Kraft_tom2,
+    G6: Kraft_noise,
+    A6: Kraft_clap,
+    "A#6": Kraft_clave,
+    C7: Indus_kick,
+    D7: Indus_tom,
+    "D#7": Indus_indus,
+    F7: Indus_scratch,
+    G7: Indus_hat,
+    A7: Indus_clap2,
+    "A#7": Indus_clap
   }).connect(this.chorus);
 
   playSound = data => {
