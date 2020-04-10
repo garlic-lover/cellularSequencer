5e548badcc8065fc7c12ffdf62cae331bfdb2078
Matthieu Poupinet <matthieupoupinet@gmail.com>
2020-04-10 21:50:12 +0200

    intro modified

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "./Components/Sider/sider.css";
 import "./Components/Synth/synth.css";
 import "./Components/Intro/tuto.css";
 import BoardContainer from "./Components/Board/BoardContainer";
+import IntroScreen from "./Components/Intro/IntroScreen";
 import TutoScreen from "./Components/Intro/TutoScreen";
 import Particles from "react-particles-js";
 
@@ -70,44 +71,7 @@ class App extends React.Component {
           </div>
           <header>The Cell Sequencer</header>
         </div>
-        <div id="screen2" className="">
-          <div className="para1">
-            <div>
-              This page is based on the concept of
-              <span className="green"> cellular </span> automatons.
-            </div>
-            <div>
-              A <span className="green"> cellular automaton</span> consists of a
-              regular grid of cells, each in one of a finite number of states.
-            </div>
-            <div>
-              Our cells can move, give<span className="green"> life</span> ,
-              make <span className="green">music</span> , and even
-              <span className="green"> die</span> <span>😢</span>
-            </div>
-          </div>
-          <div className="para2">
-            <div>
-              This page is based on the concept of
-              <span className="green"> cellular </span> automatons.
-            </div>
-            <div>
-              A <span className="green"> cellular automaton</span> consists of a
-              regular grid of cells, each in one of a finite number of states.
-            </div>
-            <div>
-              Our cells can move, give<span className="green"> life</span> ,
-              make <span className="green">music</span> , and even
-              <span className="green"> die</span> <span>😢</span>
-            </div>
-          </div>
-          <a
-            href="https://mathworld.wolfram.com/GameofLife.html"
-            target="_blank"
-          >
-            Want to know more?
-          </a>
-        </div>
+        <IntroScreen />
         <TutoScreen />
         {/*       <SiderContainer />
       <SynthContainer />
---
+++ src/Components/Intro/IntroScreen.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const IntroScreen = ({}) => {
+  return (
+    <div id="screen2" className="">
+      <div className="para1">
+        <div>
+          Welcome on the <span className="green">Cell Sequencer</span>{" "}
+        </div>
+        <div>
+          <span className="green">No need</span> for any musical education
+        </div>
+        <div>
+          Let the browser play or{" "}
+          <span className="green">control your own instruments</span> with MIDI
+        </div>
+      </div>
+      <div className="para2">
+        <div>
+          The page is based on the concept of
+          <span className="green"> cellular </span> automatons.
+        </div>
+        <div>
+          A <span className="green"> cellular automaton</span> consists of a
+          regular grid of cells, each in one of a finite number of states.
+        </div>
+        <div>
+          Our cells can move, give<span className="green"> life</span> , make{" "}
+          <span className="green">music</span> , and even
+          <span className="green"> die</span> <span>😢</span>
+        </div>
+      </div>
+      <a href="https://mathworld.wolfram.com/GameofLife.html" target="_blank">
+        Want to know more?
+      </a>
+    </div>
+  );
+};
+
+export default IntroScreen;
